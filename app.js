a70e066c28ee80bfb31c492e55815657a3007b42
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-02-25 11:34:31 -0300

    troquei a velocidade do responsive voice

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ let tentativas = 1;
 
 //Código omitido
 
-function exibirTextoNaTela(tag, texto){
+ function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto
-    responsiveVoice.speak(texto,  'Brazilian Portuguese Female', {rate: 1.2});
-}
+    responsiveVoice.speak(texto,  'Brazilian Portuguese Female', {rate: 1.0});
+
+} 
+
+
+ 
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
+
 exibirMensagemInicial();
 
 
